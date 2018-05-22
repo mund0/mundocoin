b0d3481eeed836831dc45e9e5956b3f04528d637
admin <admin@admins-MacBook-Pro.local>
2018-05-22 10:31:47 +0800

    changes

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 55883 : 12365;
+    return GetBoolArg("-testnet", false) ? 55883 : 55884;
 }
 
 Object JSONRPCError(int code, const string& message)
